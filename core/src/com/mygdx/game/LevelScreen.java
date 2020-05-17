067eac9b04200ae10ab2407971f9496994dc280b
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-05-17 09:09:39 +0300

    restart button

--- core/src/com/mygdx/game/LevelScreen.java
+++ core/src/com/mygdx/game/LevelScreen.java
@@ -3,9 +3,17 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+import java.util.concurrent.CyclicBarrier;
 
 public class LevelScreen extends BaseScreen {
 
@@ -43,15 +51,34 @@ public class LevelScreen extends BaseScreen {
             shark.setMaxSpeed(50);
         }
 
+        // player
         turtle = new Turtle(20, 20, mainStage);
-
         win = false;
         lose = false;
 
+        // UI
         starfishLabel = new Label("Starfish left:", BaseGame.labelStyle);
         starfishLabel.setColor(Color.CYAN);
         starfishLabel.setPosition(20, 520);
         uiStage.addActor(starfishLabel);
+
+        Button.ButtonStyle buttonStyle = new Button.ButtonStyle();
+        Texture buttonText = new Texture(Gdx.files.internal("undo.png"));
+        TextureRegion buttonRegion = new TextureRegion(buttonText);
+        buttonStyle.up = new TextureRegionDrawable(buttonRegion);
+        Button restartButton = new Button(buttonStyle);
+        restartButton.setStyle(buttonStyle);
+        restartButton.setColor(Color.CYAN);
+        restartButton.setPosition(720, 520);
+        uiStage.addActor(restartButton);
+
+        restartButton.addListener((Event e) -> {
+
+           if (!(e instanceof InputEvent) || !((InputEvent) e) .getType().equals(InputEvent.Type.touchDown))
+               return false;
+           StarFishGame.setActiveScreen(new LevelScreen());
+           return false;
+        });
 
     }
 
